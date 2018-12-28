eccf0c2cc4b0576f94a8a37cae2d677c4ed49490
Adam Mescher <admescher@gmail.com>
2018-12-28 13:13:48 -0700

    Content through completion of video 10 and end of module 3

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,3 +1,11 @@
 exports.homePage = (request, response) => {
     response.render('index');
 }
+
+exports.addStore = (request, response) => {
+    response.render('editStore', { title: 'Add Store' });
+}
+
+exports.createStore = (request, response) => {
+    response.json(request.body);
+}
