c5115ee51fb0c218d201017cc3a77f75586a349c
Adam Mescher <admescher@gmail.com>
2019-01-06 23:37:10 -0700

    Update through completion of video 17

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -26,6 +26,7 @@ exports.editStore = async (request, response) => {
 }
 
 exports.updateStore = async (request, response) => {
+    request.body.location.type = 'Point';
     const store = await Store.findOneAndUpdate({ _id: request.params.id }, request.body, { new: true, runValidators: true}).exec();
     request.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store →</a>`)
     response.redirect(`/stores/${store._id}/edit`);
